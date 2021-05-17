8112c4d80c298ece94013401fccbb1ef5b43e0ea
prof30 <mine4christ@gmail.com>
2021-05-17 04:06:05 -0700

    Third update

--- lib/configMaps.dart
+++
@@ -1,8 +0,0 @@
-import 'package:firebase_auth/firebase_auth.dart';
-import 'package:uber_clone/models/allUsers.dart';
-
-const String mapkey = "AIzaSyCjQd9xdJu9-wW33g1_iaJM0_haB66E6MM";
-
-User firebaseUser;
-
-Users currentUser;
